4b979b086091e7ac6aa40f1b5f2ba9fba8a41f72
Thiago Thomas <thiago.santos@truechange.com>
2025-10-16 23:30:18 +0100

    creation of Layout and the Homepage

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,38 @@
+import './styles.css'
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+export function Layout({ children }: LayoutProps) {
+  return (
+    <div className='layout'>
+      <header className="layout__header">
+        <div className="container">
+          <nav className="layout__nav">
+            <div className="layout__brand">
+              <a href="/" className="layout__logo">
+                Apiki Blog 
+              </a>
+              <span className="layout__subtitle">Development</span>
+            </div>
+          </nav>
+        </div>
+      </header>
+
+      <main className="layout__main">
+        {children}
+      </main>
+
+      <footer className="layout__footer">
+        <div className="container">
+          <div className="layout__footer-content">
+            <p className="layout__footer-text">
+              © 2025 Apiki Blog. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  )
+}
